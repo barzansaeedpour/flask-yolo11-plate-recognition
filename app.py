1a86b24c46519d75135152300d55f8ad4a461753
barzansaeedpour <barzansaeedpour@gmail.com>
2025-05-18 15:01:22 +0330

    added testing images

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import cv2
 from werkzeug.utils import secure_filename
 from my_yolo_11.yolov11 import plate_detection
 from ultralytics import YOLO
+from PIL import Image
+import io
+import numpy as np
 
 app = Flask(__name__)
 app.secret_key = "change_this_to_a_random_secret"  # required for flash()
@@ -108,7 +111,6 @@ def upload_video():
     # camera = request.args.get('camera', 'false').lower() == 'true'
     return render_template('upload_video.html')
 
-
 @app.route('/detect_video')
 def detect_video():
     # camera = request.args.get('camera', 'false').lower() == 'true'
@@ -116,6 +118,49 @@ def detect_video():
         flash('اول باید یک ویدیو آپلود کنید')
         return redirect(url_for('index'))
     return render_template('video.html', context={"camera":False})
+
+@app.route('/upload_image')
+def upload_image():    
+    return render_template('image.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    # Read and process the uploaded image
+    file = request.files['image']
+    image = Image.open(io.BytesIO(file.read()))
+    frame = np.array(image)
+    # frame = frame[:,:,[2,1,0]]
+    frame = frame[:,:,[0,1,2]]
+    
+    detected_plate_txt, detected_plate_image, detected_chars_image = plate_detection(frame, model_plate_detection, model_character_detection, save_dir='', save=False)
+
+    if detected_plate_txt!='':
+        cv2.imwrite('./detected_plate_image.png', detected_plate_image)
+        cv2.imwrite('./detected_chars_image.png', detected_chars_image)
+        # Save images to in-memory files to send back as response 
+        detected_chars_img_io = io.BytesIO() 
+        Image.fromarray(detected_chars_image).save(detected_chars_img_io, 'PNG') 
+        detected_chars_img_io.seek(0) 
+        detected_plate_img_io = io.BytesIO() 
+        Image.fromarray(detected_plate_image).save(detected_plate_img_io, 'PNG') 
+        detected_plate_img_io.seek(0)
+    
+        # Perform detection
+        # results = model(image)
+        # detections = results.pandas().xyxy[0].to_dict(orient='records')  # List of detection dictionaries
+
+        # # return jsonify({'detections': "detected_plate_txt"})
+        # detected_plate_txt = detected_plate_txt[:-2] + ' ' + detected_plate_txt[-2:]
+        return jsonify({ 'detected_plate_txt': detected_plate_txt, 'detected_chars_image': 'data:image/png;base64,' + base64.b64encode(detected_chars_img_io.getvalue()).decode(), 'detected_plate_image': 'data:image/png;base64,' + base64.b64encode(detected_plate_img_io.getvalue()).decode() })
+    else:
+        return jsonify({ 'detected_plate_txt': 'عدم تشخیص', 'detected_chars_image': '', 'detected_plate_image': '' })
+        
+        
+
+
 
 
 @app.route('/video_feed')
